6fa9c80b9e34e28c501042825e516a2a6b55c7ee
Khaliq Noor <khaliqnoor369@gmail.com>
2025-10-28 23:33:51 -0700

    Email Reminder Added

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -3,6 +3,8 @@ await connectDB();
 
 import { Inngest } from "inngest";
 import User from "../models/User.js";
+import Connection from "../models/Connections.js";
+import sendEmail from "../configs/nodeMailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "Nexi" });
@@ -56,11 +58,58 @@ const syncUserDeletion = inngest.createFunction(
     }     
 )
 
+// Inngest function to send reminder when a new connnection request is sent or added 
+const sendNewConnectionRequestReminder = inngest.createFunction(
+    {id: "send-new-connection-request-reminder"},
+    {event: "app/connection-request"},
+    async ({event, step}) => {
+        const {connectionId} = event.data;
 
+        await step.run('send-connection-request-mail', async () => {
+            const connection = await Connection.findById(connectionId).populate('from_user_id to_user_id');
+            const subject = '👋 New Connection Request'
+            const body = `<div style="font-family: Arial, sans-serif; background-color:#f9fafb; padding:20px; border-radius:10px;">
+            <h2 style="color:#333;">Hi ${connection.to_user_id.fullName},</h2>
+        <p> You have a new connection request from ${connection.from_user_id.fullName} - ${connection.from_user_id.username} </p>
+            <p> Click <a href="${process.env.FRONTEND_URL}/connections" style="color:#10b981">here</a>to accept or reject the request</p>
+            <br/>
+            <p>Thanks, <br/>Nexi - Stay Connected
+            </div>`;
+            await sendEmail({
+                to: connection.to_user_id.email,
+                subject,
+                body
+            })
+       })
+       const in24Hours = new Date(Date.now() + 24*60*60 * 1000)
+       await step.sleepUntil('wait-for-24-hours', in24Hours)
+       await step.run('send-connection-request-reminder', async () => {
+          const connection = await Connection.findById(connectionId).populate('from_user_id to_user_id');
+          if(connection.status === 'accepted'){
+            return {message: "Already accepted"}
+          }
+         const subject = '👋 New Connection Request'
+            const body = `<div style="font-family: Arial, sans-serif; background-color:#f9fafb; padding:20px; border-radius:10px;">
+            <h2 style="color:#333;">Hi ${connection.to_user_id.fullName},</h2>
+        <p> You have a new connection request from ${connection.from_user_id.fullName} - ${connection.from_user_id.username} </p>
+            <p> Click <a href="${process.env.FRONTEND_URL}/connections" style="color:#10b981">here</a>to accept or reject the request</p>
+            <br/>
+            <p>Thanks, <br/>Nexi - Stay Connected
+            </div>`;
+            await sendEmail({
+                to: connection.to_user_id.email,
+                subject,
+                body
+            })
+            return {message: "Reminder Sent"}
+       })
+    }
+)
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [
     syncUserCreation,
     syncUserUpdation,
-    syncUserDeletion
+    syncUserDeletion,
+    sendNewConnectionRequestReminder
 ];
--- server/server.js
+++ server/server.js
@@ -4,17 +4,25 @@ import 'dotenv/config'
 import connectDB from './configs/db.js'
 import {inngest, functions} from './inngest/index.js'
 import {serve} from 'inngest/express'
+import { clerkMiddleware } from '@clerk/express'
+import userRouter from './routes/userRoutes.js'
 
 const app = express()
 await connectDB()
 
+
+// The below clerkmiddleware will add the auth property when the user is authenticated like login or not 
+
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
 app.get('/', (req, res) => {
   res.send('Hello World!, how are you all doing is everything all right')
 })
 app.use('/api/inngest', serve({ client: inngest, functions }))
+app.use('/api/user', userRouter)
+
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
